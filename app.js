4f71c90deffcc87fbe28b396cb172d2ab172ce90
iaarchiarora <98007567+iaarchiarora@users.noreply.github.com>
2024-05-03 11:53:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-console.log("app.js file");
+// console.log("app.js file");
+// console.log(new Date());
+// <<<<<<< HEAD
+// =======
+// console.log("edited in browser");
+
+// >>>>>>> refs/remotes/origin/main
+
+console.log("app.js file for node");
 console.log(new Date());
-<<<<<<< HEAD
-=======
-console.log("edited in brower");
->>>>>>> refs/remotes/origin/main
+console.log("Added Line 3");
+console.log("edited in browser");
+const app=require('express');
+app.length('/',(req,res)=>{
+    res.json({message:"server running"});
+});
+app.listen(700,()=>{
+    console.log("server running on 7000");
+});
